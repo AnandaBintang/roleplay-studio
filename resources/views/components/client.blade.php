0db6db1ce64ea7714ee595eaf180893c15d1d495
AnandaBintang <anandabintang4@gmail.com>
2023-12-08 00:54:10 +0700

    fix client image not responsive

--- resources/views/components/client.blade.php
+++ resources/views/components/client.blade.php
@@ -4,8 +4,11 @@
     @foreach ($clients as $client)
         <div class="w-1/4 p-3 md:p-6" data-aos="zoom-in">
             <div
-                class="client w-16 h-16 md:w-32 md:h-32 xl:w-28 xl:h-28 2xl:w-36 2xl:h-36 rounded-2xl md:rounded-3xl flex items-center justify-center">
-                <img src="{{ asset('storage/' . $client['logo']) }}" alt="{{ $client['client_name'] }}">
+                class="client w-16 h-16 md:w-32 md:h-32 xl:w-28 xl:h-28 2xl:w-36 2xl:h-36 rounded-2xl md:rounded-3xl flex items-center justify-center relative">
+                @isset($client['logo'])
+                    <img src="{{ asset('storage/' . $client['logo']) }}" alt="{{ $client['client_name'] }}"
+                        class="object-scale-down p-4 object-center w-auto h-auto">
+                @endisset
             </div>
         </div>
     @endforeach
